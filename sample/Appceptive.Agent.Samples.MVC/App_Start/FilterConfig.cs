5aee5c201566f4b73077383ca568eb94859b0f40
Stefan Sedich <stefan.sedich@gmail.com>
2014-03-18 17:59:57 +1000

    include samples with agent

---
+++ sample/Appceptive.Agent.Samples.MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Appceptive.Agent.Samples.MVC
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ sample/Appceptive.Agent.Samples.MVC/Global.asax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Appceptive.Agent.Serilog;
+using Serilog;
+
+namespace Appceptive.Agent.Samples.MVC
+{
+	public class MvcApplication : System.Web.HttpApplication
+	{
+        protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+			Core.Appceptive.Start(c =>
+			{
+				c.ActivityDispatchInterval = TimeSpan.FromSeconds(5);
+
+                c.Filter(activity =>
+                {
+                    var url = (string) activity.Properties["Url"];
+
+                    return url.Contains("/Images/");
+                });
+			});
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .WriteTo.Appceptive()
+                .CreateLogger();
+		}
+	}
+}
